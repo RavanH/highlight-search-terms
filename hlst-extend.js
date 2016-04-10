bb898c2914da6349e325119196943a4141bb4bdb
RavanH <ravanhagen@gmail.com>
2016-04-10 23:02:36 +0200

    improved click-through highlighting + bbpress comp

--- hlst-extend.js
+++ hlst-extend.js
@@ -1,3 +1,107 @@
-(function(f){f.fn.highlight=function(a,c,b,e){return this.each(function(){b=b||"mark";e=e||"hilite";var d=this.firstChild,g,k,l=[];g="SCRIPT STYLE INPUT SELECT BUTTON OBJECT APPLET TEXTAREA PRE CODE EMBED IFRAME".split(" ");if(a&&d&&-1==f.inArray(this.nodeName,g)){var h=new RegExp(a.replace(/([-.*+?^${}()|[\]\/\\])/g,"\\$1"),c?"ig":"g");do 3===d.nodeType&&(g=d.nodeValue,k=g.replace(h,function(a){return"<"+b+' class="'+e+'">'+a+"</"+b+">"}),k!==g&&(f(d).before(k),l.push(d)));while(d=d.nextSibling)}l.length&&
-f(l).remove()})};if("undefined"!=typeof hlst_query){if(0==hlst_query.length){var c=document.referrer.split("?");if("undefined"!=typeof c[1]){var a;a=9>document.referrer.indexOf(document.domain)?"s":-1<document.referrer.indexOf("yahoo.com")?"p":-1<document.referrer.indexOf("goodsearch.com")?"keywords":-1<document.referrer.indexOf("mywebsearch.com")?"searchfor":-1<document.referrer.indexOf("baidu.")?"wd":"q";for(var c=c[1].split("&"),b=0;b<c.length;b++){var e=c[b].indexOf("=");if(0<e&&a==c[b].substring(0,
-e)){qstr=decodeURIComponent((c[b].substring(e+1)+"").replace(/\+/g,"%20"));qarr=qstr.match(/([^\s"]+)|"([^"]*)"/g)||[];for(a=0;a<qarr.length;a++)hlst_query[a]=qarr[a].replace(/"/g,"");break}}}}var h,c=jQuery.support.opacity?"mark":"span";if(0!=hlst_query.length)for(h in hlst_areas)if(a=f(hlst_areas[h]),0!=a.length){for(b in hlst_query)a.highlight(hlst_query[b],!0,c,"hilite term-"+b),a.find("*").highlight(hlst_query[b],!0,c,"hilite term-"+b);break}}"function"==typeof Cufon&&Cufon.refresh()})(jQuery);
+(function($){
+	$.fn.highlight = function( term, insensitive, t , c ) {
+		return this.each(function(){
+			t = t || 'mark';
+			c = c || 'hilite';
+			var node = this.firstChild,
+			val,
+			new_val,
+			remove = [],
+			skip = ['SCRIPT', 'STYLE', 'INPUT', 'SELECT', 'BUTTON', 'OBJECT', 'APPLET', 'TEXTAREA', 'PRE', 'CODE', 'EMBED', 'IFRAME'];
+
+			if ( term && node && $.inArray(this.nodeName, skip) == -1 ) {
+				var regex = new RegExp( term.replace(/([-.*+?^${}()|[\]\/\\])/g,"\\$1"), insensitive ? 'ig' : 'g');
+
+				do {
+					if ( node.nodeType === 3 ) {
+						val = node.nodeValue;
+						new_val = val.replace( regex, function(x){
+							return '<' + t + ' class="' + c + '">' + x + '</' + t + '>';
+						} );
+						if ( new_val !== val ) {
+							$(node).before( new_val );
+							remove.push( node );
+						}
+					}
+				} while ( node = node.nextSibling );
+			}
+
+			remove.length && $(remove).remove();
+		});
+	};
+
+	if (typeof(hlst_query) != 'undefined') {
+		if (hlst_query.length == 0) {
+			var ref = document.referrer.split('?');
+			/*
+			console.log('referrer query parameters: ' + ref[1]);
+			*/
+			if (typeof(ref[1]) != 'undefined') {
+				var term;
+				if (document.referrer.indexOf('yahoo.com') > -1) {
+					term = 'p';
+				} else if (document.referrer.indexOf('goodsearch.com') > -1) {
+					term = 'keywords';
+				} else if (document.referrer.indexOf('mywebsearch.com') > -1) {
+					term = 'searchfor';
+				} else if (document.referrer.indexOf('baidu.') > -1) {
+					term = 'wd';
+				} else {
+					term = 'q';
+				}
+				/*
+				console.log('searchengine term: ' + term);
+				*/
+				var parms = ref[1].split('&');
+				/*
+				console.log('parms split into ' + parms.length);
+				*/
+				for (var i=0; i < parms.length; i++) {
+					/*
+					console.log('parameter ' + i + ': ' + parms[i]);
+					*/
+					var pos = parms[i].indexOf('=');
+					if (pos > 0) {
+						if(term == parms[i].substring(0,pos)) {
+							qstr = decodeURIComponent((parms[i].substring(pos+1)+'').replace(/\+/g, '%20'));
+							/*
+							console.log('search query found: ' + qstr);
+							*/
+							qarr = qstr.match(/([^\s"]+)|"([^"]*)"/g) || [];
+							for (var j=0; j < qarr.length; j++){
+								/*
+								console.log('added ' + qarr[j] + ' to search array');
+								*/
+								hlst_query[j] = qarr[j].replace(/"/g,'');
+							}
+							break;
+						}
+					}
+				}
+			}
+		}
+
+		var area, i, s;
+		var t = jQuery.support.opacity ? 'mark' : 'span';
+
+		if (hlst_query.length != 0) {
+			for (s in hlst_areas){
+				area = $(hlst_areas[s]);
+				/*
+				console.log('testing area: ' + hlst_areas[s]);
+				*/
+				if (area.length != 0){
+					for (i in hlst_query){
+						/*
+						console.log('searching for: ' + hlst_query[i]);
+						*/
+						area.highlight(hlst_query[i], true, t, 'hilite term-' + i);
+						area.find('*').highlight(hlst_query[i], true, t, 'hilite term-' + i)
+					}
+					break;
+				}
+			}
+		}
+	}
+	if ('function'==typeof Cufon) Cufon.refresh();
+})(jQuery);
