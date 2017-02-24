f885e6b3f11f41a846e86af8418cda6e16e6f87b
RavanH <ravanhagen@gmail.com>
2017-02-24 16:51:04 +0100

    copyleft year date

--- hlst.php
+++ hlst.php
@@ -9,7 +9,7 @@ Author URI: http://status301.net/
 Text Domain: highlight-search-terms
 */
 
-/*  Copyright 2016  RavanH  (email : ravanhagen@gmail.com)
+/*  Copyright 2017  RavanH  (email : ravanhagen@gmail.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
