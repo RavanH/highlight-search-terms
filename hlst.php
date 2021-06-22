fdb71bb27667b09c6871da8f8071b0b9ecdd866d
RavanH <ravanhagen@gmail.com>
2021-06-22 16:32:27 +0200

    veresion 1.6

--- hlst.php
+++ hlst.php
@@ -3,7 +3,7 @@
 Plugin Name: Highlight Search Terms
 Plugin URI: http://status301.net/wordpress-plugins/highlight-search-terms
 Description: Wraps search terms in the HTML5 mark tag when referrer is a non-secure search engine or within wp search results. Read <a href="http://wordpress.org/extend/plugins/highlight-search-terms/other_notes/">Other Notes</a> for instructions and examples for styling the highlights. <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=ravanhagen%40gmail%2ecom&item_name=Highlight%20Search%20Terms&no_shipping=0&tax=0&bn=PP%2dDonationsBF&charset=UTF%2d8&lc=us" title="Thank you!">Tip jar</a>.
-Version: 1.6-alpha-1
+Version: 1.6
 Author: RavanH
 Author URI: http://status301.net/
 Text Domain: highlight-search-terms
@@ -43,7 +43,7 @@ class HighlightSearchTerms {
 	*/
 
 	// plugin version
-	private static $version = '1.6-alpha-1';
+	private static $version = '1.6';
 
 	// filtered search terms
 	private static $search_terms = null;
